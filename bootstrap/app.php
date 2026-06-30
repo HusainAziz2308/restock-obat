b8f9ccefa695f1fc15a6f812a7a1f3fc6ca6d13b
HusainAziz2308 <husainazizalrosyid@gmail.com>
2026-06-30 20:15:06 +0700

    fQeawh

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,18 +8,27 @@ use \App\Http\Middleware\EnsurePharmacyIsSetup;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        $middleware->redirectGuestsTo(fn () => route('filament.admin.auth.login'));
+        $middleware->redirectGuestsTo(fn() => route('filament.admin.auth.login'));
         $middleware->web(append: [
             EnsurePharmacyIsSetup::class,
         ]);
+
+        $middleware->trustedProxies(
+            '*',
+            Request::HEADER_X_FORWARDED_FOR |
+            Request::HEADER_X_FORWARDED_HOST |
+            Request::HEADER_X_FORWARDED_PORT |
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_AWS_ELB
+        );
     })
     ->withProviders([
-       AdminPanelProvider::class,
+        AdminPanelProvider::class,
     ])
     ->withExceptions(function (Exceptions $exceptions): void {
         //
